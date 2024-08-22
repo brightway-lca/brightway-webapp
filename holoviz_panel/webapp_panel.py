619c6698911fe8b55f5825b31922059cdb82dae8
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2024-08-22 15:50:05 +0200

    added mwe

---
+++ holoviz_panel/webapp_panel.py
@@ -0,0 +1,44 @@
+# %%
+import panel as pn
+pn.extension()
+
+import bw2data as bd
+import bw2io as bi
+import bw2calc as bc
+
+# minimum working example of a
+# life-cycle assessment calculation with Brightway
+
+def recreate_fresh_project(project_name):
+    try:
+        bd.projects.delete_project(project_name, delete_dir=True)
+    except:
+        pass
+    bd.projects.create_project(project_name)
+    bd.projects.set_current(project_name)
+
+recreate_fresh_project(project_name = "bw_panel")
+
+bi.add_example_database(searchable=False)
+co2 = bd.get_node(name="CO2")
+gwp = bd.Method(("GWP", "simple"))
+gwp.write([(co2.key, 1)])
+lca = bc.LCA({bd.get_node(name="Electric car"):1}, method=("GWP", "simple"))
+lca.lci()
+lca.lcia()
+lca_score = lca.score
+
+# minimum working example of a
+# Panel app with a button and a text widget
+
+dial = pn.indicators.Dial(
+    name='CO2 [kg]',
+    value=lca_score,
+    format='{value:,.0f}kg',
+    bounds=(0, 5000)
+)
+
+# https://panel.holoviz.org/reference/layouts/Column.html
+pn.Column(
+    dial
+).servable()
