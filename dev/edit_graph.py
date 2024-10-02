269bc7007eb963a08cd18223806f909962b3cb01
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2024-10-02 15:54:17 +0200

    initial commit

--- dev/edit_graph.py
+++ dev/edit_graph.py
@@ -1,12 +1,31 @@
+# %%
 import pandas as pd
 
-# %%
-
-data = {
+data_original = {
     'uid': [0, 1, 2, 3],
     'description': ['cardboard', 'paper', 'wood', 'electricity'],
-    'production': [True, False, True, False],
+    'production': [1, 0.5, 0.2, 0.1],
     'emissions_intensity': [1.5, 2.2, 0.7, 0.2],
+    'branch': [[], [0,1], [0,1,2], [0,1,2,3]]
 }
 
-df = pd.DataFrame(data)
+df_original = pd.DataFrame(data_original)
+
+def compute_direct_emissions(df):
+    df['direct_emissions'] = df['production'] * df['emissions_intensity']
+    return df
+
+data_after_user_input = {
+    'uid': [0, 1, 2, 3],
+    'description': ['cardboard', 'paper', 'wood', 'electricity'],
+    'production': [1, 0.25, 0.2, 0.1],
+    'production_override': [False, True, False, False],
+    'emissions_intensity': [1.5, 2.2, 0.1, 0.2],
+    'emissions_intensity_override': [False, False, True, False],
+    'branch': [[], [0,1], [0,1,2], [0,1,2,3]]
+}
+
+df_after_user_input = pd.DataFrame(data_after_user_input)
+
+
+# %%
